60b01611a027e8783e1f2766b3cbb527130f440f
ZedZedZee <zemp1@umbc.edu>
2014-10-16 21:24:54 -0400

    Added some time stamp code

--- CoffeeApplication.java
+++ CoffeeApplication.java
@@ -1,8 +1,21 @@
-// list imports
 import java.util.Scanner;
 
 public class CoffeeApplication
 {
+	//Type "printTimeStamp()" to time stamp tastings/edits and reports
+   //The hour is not printing correctly. Anyone see the mistake? I don't. Java book pg 52-53
+public static void printTimeStamp () 
+   {
+      long totalMilliseconds = System.currentTimeMillis();
+      long totalSeconds = totalMilliseconds / 1000;
+      long currentSecond = totalSeconds % 60;
+      long totalMinutes = totalSeconds / 60;
+      long currentMinute = totalMinutes % 60;
+      long totalHours = totalMinutes / 60;
+      long currentHour = totalHours % 24; 
+      System.out.println(currentHour + ":" + currentMinute + ":" + currentSecond); 
+   }
+
 	public static void main (String [] args)
 	{
 		Scanner input = new Scanner(System.in);
@@ -15,7 +28,6 @@ public class CoffeeApplication
 		4. Else - exit program	
 	We may choose to turn these into methods.
 */
-
       System.out.print("Do you want to create a new tasting report? Enter Y or N: ");
       String start = input.next();
       if (start.equalsIgnoreCase("Y"))
@@ -46,6 +58,11 @@ public class CoffeeApplication
              }
       }
       
+// All sections below will need to be cut/paste into the if/else stmts above.
+
+// Assignments are suggestive just to get started. 
+      //Take away the ? mark to accept the task      
+      //Delete your name if you don't want the task
       
 // New Tasting
 	/* MICHELLE? - Prompt/Enter Name
@@ -59,17 +76,18 @@ public class CoffeeApplication
          				4. Country
 	*/
 	
-	/*	ARE WE DOING THIS? While loop to allow multiple cups in one sitting?
-			                  Prompt for number of cups
-			                  Initialize count to 1, count to <=number of cups and increment by 1
-		                  	Are there unforeseen consequences to looping this?
+	/*	ARE WE DOING THIS?   While loop - to allow multiple cups in one sitting?
+                           - We can always add this in later.
+			                  - Prompt for number of cups
+			                  - Initialize count to 1, count to <=number of cups and increment by 1
+		                  	- Are there unforeseen consequences to looping this?
 	*/
 	
 	/* MICHELLE? - Prompt/Enter Bean type - String
-                Prompt for brewing method - or use if else to ask if drip/pour over/french press/cold brew/ iced
+                  Prompt for brewing method - or use if else to ask if drip/pour over/french press/cold brew/ iced
 	
-	             Prompt/Enter price (double) & currency 
-	             Currency class - convert to U.S. dollar
+	               Prompt/Enter price (double) & currency 
+	               Currency class - convert to U.S. dollar
 	*/
 	
 	/*	ANDREA - If it is a hot coffee (see brewing method) run the following : 
@@ -81,20 +99,21 @@ public class CoffeeApplication
 	*/
 	
 	/*MICHELLE?- 1. Prompt for int ratings w/option to skip a category
-			         1. Bitterness 2. Acidity 3. Fragrance 4. Flavor 
-			         5. Aftertaste 6. Body 7. Sweetness 8. 'Clean Cup'
+			             1. Bitterness 2. Acidity 3. Fragrance 4. Flavor 
+			             5. Aftertaste 6. Body 7. Sweetness 8. 'Clean Cup'
 		          2. Sum scores and divide by the number of not skipped categories
 	*/
 	
-	// ANDREA tasting notes
-	
-	/*ANDREA - time stamp (Use System.currentTimemillis();
-		divide by 1000
-		continue with / & % 60 until current time is deduced
-		format time
-	*/
-	
-	/*ANDREA -  Save text file
+	// Tasting notes
+   System.out.print("Enter Tasting Notes (press Enter to submit notes): ");
+   //The next line of code was necessary in order to make nextLine() work after using next() above. 
+   input.nextLine();
+   String tastingNotes = input.nextLine();
+   
+	//ANDREA timestamp
+   printTimeStamp();
+   
+   /*ANDREA -  Save text file
 			1. User specifies directory
 			2. User specified file name
 	*/
@@ -109,14 +128,14 @@ public class CoffeeApplication
 
 // PIERCE? (This is a large section. Could you start it and we'll help as we finish our sections)
 // Generate Reports
-	//if/else to determine which report type they wish to generate
-	//Prompt for time period
-	/* 1. where drank most coffee
-			by coffee shop
-			by geographical location*/
-	// 2. How many cups of coffee has user consumed
-	// 3. How much user spent on coffee
-	// 4. Coffee ratings (highest & lowest)
+	// If/else to determine which report type they wish to generate
+	// Prompt for time period
+	/*     1. where drank most coffee
+			      by coffee shop
+			      by geographical location*/
+	//     2. How many cups of coffee has user consumed
+	//     3. How much user spent on coffee
+	//     4. Coffee ratings (highest & lowest)
 			
 	
 			
